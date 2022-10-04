b1de4c30c9f5ac7237362b296b3cbb9439367502
sakthivelanm <sakthivelanm@gmail.com>
2022-10-04 15:29:17 -0400

    Chapter 6

---
+++ api/src/services/comments/comments.test.js
@@ -0,0 +1,32 @@
+import { comments, createComment } from './comments'
+
+describe('comments', () => {
+  scenario('returns all comments for a single post from db', async (scenario) => {
+    const result = await comments({ postId: scenario.comment.jane.postID })
+    const post = await db.post.findUnique({
+      where: { id: scenario.comment.jane.postId },
+      include: { commennts: true },
+    })
+    expect(result.length).toEqual(post.comments.length)
+  })
+
+    expect(result.length).toEqual(Object.keys(scenario.comment).length)
+  })
+
+  scenario('postOnly', 'creates a new comment', async (scenario) => {
+    const comment = await createComment({
+      input: {
+        name: 'Billy Bob',
+        body: 'What is your favorite tree bark?',
+        post: {
+          connect: { id: scenario.post.bark.id },
+        },
+      },
+    })
+
+    expect(comment.name).toEqual('Billy Bob')
+    expect(comment.body).toEqual('What is your favorite tree bark?')
+    expect(comment.postId).toEqual(scenario.post.bark.id)
+    expect(comment.createdAt).not.toEqual(null)
+  })
+})
